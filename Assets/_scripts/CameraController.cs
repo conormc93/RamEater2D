a603a5957d2a8a08f6be08b1a84ffde370c5a538
conormc93 <conordmcgrath93@gmail.com>
2018-10-23 23:56:37 +0100

    Fixed Camera Bug

---
+++ Assets/_scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public PlayerController player;
+    public bool isFollowing;
+
+    public float xOffSet;
+    public float yOffSet;
+
+    // Use this for initialization
+    void Start () {
+        player = FindObjectOfType<PlayerController>();
+
+        isFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFollowing)
+        {
+            transform.position = new Vector3(player.transform.position.x + xOffSet, player.transform.position.y + yOffSet, transform.position.z);
+        }
+	}
+}
--- Assets/_scripts/LevelManager.cs
+++ Assets/_scripts/LevelManager.cs
@@ -8,15 +8,20 @@ public class LevelManager : MonoBehaviour {
 
 	private PlayerController player;
 
+    private float gravityStore;
+
 	//animation
 	public GameObject deathParticle;
 	public GameObject respawnParticle;
 
 	public float respawnDelay;
 
+    public CameraController camera;
+
 	// Use this for initialization
 	void Start () {
-		player = FindObjectOfType<PlayerController>();
+        player = FindObjectOfType<PlayerController>();
+        camera = FindObjectOfType<CameraController>();
 	}
 	
 	// Update is called once per frame
@@ -29,18 +34,41 @@ public class LevelManager : MonoBehaviour {
 		StartCoroutine ("RespawnPlayerCo");
 	}
 
-	public IEnumerator RespawnPlayerCo()
-	{
-		Instantiate(deathParticle, player.transform.position, player.transform.rotation);
-		player.enabled = false;
-		player.GetComponent<Renderer> ().enabled = false;
-		Debug.Log("Player Respawn");
-		yield return new WaitForSeconds(respawnDelay);
+    public IEnumerator RespawnPlayerCo()
+    {
+        Debug.Log("DEBUG : Player Respawn");
 
-		player.transform.position = currentCheckPoint.transform.position;
-		player.enabled = true;
-		player.GetComponent<Renderer> ().enabled = true;
-		Instantiate(respawnParticle, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
-	}
+        // Instantiate death particle where player died
+        Instantiate(deathParticle, player.transform.position, player.transform.rotation);
+
+        // Disable the player while respawning
+        player.enabled = false;
+        player.GetComponent<Renderer>().enabled = false;
+        camera.isFollowing = false;
+
+        // remove gravity when player dies
+        //gravityStore = player.GetComponent<Rigidbody2D>().gravityScale;
+        //player.GetComponent<Rigidbody2D>().gravityScale = 0f;
+
+        // stop player when respawn
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        // Deduct points from score
+        //ScoreManager.AddPoints(-deathPenalty);
+
+        yield return new WaitForSeconds(respawnDelay);
+
+        //reset player gravity
+        //player.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
+        player.transform.position = currentCheckPoint.transform.position;
+
+        // Re enable player
+        player.enabled = true;
+        player.GetComponent<Renderer>().enabled = true;
+        camera.isFollowing = true;
+        // Instantiate respawn particle where player respawns
+        Instantiate(respawnParticle, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
+
+    }
 
 }
