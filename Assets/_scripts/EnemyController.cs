e6a09695e76aec05e64c97bdb285ed80c54bab29
conormc93 <conordmcgrath93@gmail.com>
2018-10-23 22:30:25 +0100

    Enemy Animation & Script

--- Assets/_scripts/EnemyController.cs
+++ Assets/_scripts/EnemyController.cs
@@ -8,8 +8,18 @@ public class EnemyController : MonoBehaviour {
 
     public float moveSpeed;
     public bool moveRight;
-	// Use this for initialization
-	void Start () {
+
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool hittingWall;
+
+    private bool notAtEdge;
+    public Transform edgeCheck;
+
+
+    // Use this for initialization
+    void Start () {
 
         rigid2D = GetComponent<Rigidbody2D>();
 
@@ -18,13 +28,23 @@ public class EnemyController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
+
+        if (hittingWall || !notAtEdge)
+        {
+            moveRight = !moveRight;
+        }
+
         if (moveRight)
         {
-
+            transform.localScale = new Vector3(-1f, 1f, 1f);
             rigid2D.velocity = new Vector2(moveSpeed, rigid2D.velocity.y);
         }
         else
         {
+            transform.localScale = new Vector3(1f, 1f, 1f);
             rigid2D.velocity = new Vector2(-moveSpeed, rigid2D.velocity.y);
         }
 		
