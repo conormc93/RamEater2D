e0cfe9d9d6302e4d56fddfc4836e2668bd46e16b
conormc93 <conordmcgrath93@gmail.com>
2018-11-10 05:34:57 +0000

    Created Home Menu

---
+++ Assets/_scripts/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public string startLevel;
+
+    public string levelChoice; 
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(startLevel);
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(levelChoice);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
